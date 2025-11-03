bd516b924a4c6869120d9501e26593ff461d7ea3
Magicorn <lemagicorn@gmail.com>
2025-11-03 15:27:39 +0100

    Opsætning af opgaveskift

---
+++ Workspace Razor/wwwroot/js/OpgaveSkift.js
@@ -0,0 +1,17 @@
+﻿function show(type) {
+    const box = document.getElementById('opgaveBox');
+    switch (type) {
+        case 'one':
+            box.innerHTML = "<p>Opgave 1</p>";
+            break;
+        case 'two':
+            box.innerHTML = "<p>Opgave 2</p>";
+            break;
+        case 'three':
+            box.innerHTML = "<p>Opgave 3</p>";
+            break;
+        default:
+            box.innerHTML = "<p>Error</p>";
+            break;
+    }
+}
