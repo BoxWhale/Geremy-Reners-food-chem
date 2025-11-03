ab80e4fe5f25caa1bc68f9f7ebd0d8497d17f9d6
Magicorn <lemagicorn@gmail.com>
2025-11-03 11:46:58 +0100

    Simpel sidebar menu og opgavebox
    Har sat simpel kode ind til en sidebar med knapper til at skifte opgaver, og en boks til at vise opgaverne og linked en JS fil:
    Workspace MVC/Pages/TestTabs/Tab1.cshtml
    
    JS filen findes i
    Workspace MVC/wwwroot/js/OpgaveSkift.js
    
    Tilføjet lidt CSS i form af .sidebar og .opgaveBox til:
    Workspace MVC/wwwroot/css/site.css
    
    Kan ikke uploade filer fra:
    Workspace MVC/.vs/Workspace MVC/FileContentIndex/
    Så hvis noget ikke virker, er det måske derfor.

--- Workspace MVC/obj/Release/net9.0/Workspace MVC.AssemblyInfo.cs
+++ Workspace MVC/obj/Release/net9.0/Workspace MVC.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Workspace MVC")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41c6a15cbd433353082b47ae23d6fe670ac2e4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Workspace MVC")]
