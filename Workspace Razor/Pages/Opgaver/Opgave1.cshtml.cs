fa51ffe3adcfce2d9416ac814c3ae2d511814a26
Happy_Majima <62021893+BoxWhale@users.noreply.github.com>
2025-11-19 18:33:53 +0100

    Add Opgave1 Razor page and update sidebar navigation
    Created Opgave1.cshtml.cs with a corresponding Razor page and model. Updated Tab1.cshtml to improve sidebar navigation, standardize button attributes, and enhance script loading for task selection.

---
+++ Workspace Razor/Pages/Opgaver/Opgave1.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Workspace_Razor.Pages.Opgaver
+{
+    public class Opgave1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+
+}
