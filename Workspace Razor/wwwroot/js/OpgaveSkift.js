a37206447a5d72df820fc5e39e3acd5f3ae35260
Magicorn <lemagicorn@gmail.com>
2025-11-19 15:59:18 +0100

    Opgaveskift test

--- Workspace Razor/wwwroot/js/OpgaveSkift.js
+++ Workspace Razor/wwwroot/js/OpgaveSkift.js
@@ -1,17 +1,25 @@
 ﻿function show(type) {
-    const box = document.getElementById('opgaveBox');
-    switch (type) {
-        case 'one':
-            box.innerHTML = "<p>Opgave 1</p>";
-            break;
-        case 'two':
-            box.innerHTML = "<p>Opgave 2</p>";
-            break;
-        case 'three':
-            box.innerHTML = "<p>Opgave 3</p>";
-            break;
-        default:
-            box.innerHTML = "<p>Error</p>";
-            break;
-    }
+    fetch(type)
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('opgaveBox').innerHTML = html;
+        })
+        .catch (error => { console.log('Error fetching content:', error); });
 }
+
+
+//const box = document.getElementById('opgaveBox');
+//switch (type) {
+//    case 'one':
+//        box.innerHTML = document.getElementById(opgave1A);
+//        break;
+//    case 'two':
+//        box.innerHTML = "<p>Opgave 2</p>";
+//        break;
+//    case 'three':
+//        box.innerHTML = "<p>Opgave 3</p>";
+//        break;
+//    default:
+//        box.innerHTML = "<p>Error</p>";
+//        break;
+//}
