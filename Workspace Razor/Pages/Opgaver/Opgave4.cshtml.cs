3f18a54a322c8e0985a37dc4b4feea593fc314c3
vippermand <truelsandthetruels@gmail.com>
2025-12-01 13:45:25 +0100

    Opgaver sat ind

---
+++ Workspace Razor/Pages/Opgaver/Opgave4.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+// IGNORE
+// This is a basic template file. Do not delet this file
+namespace Workspace_Razor.Pages.Opgaver
+{
+    public class Opgave4Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+
+}
