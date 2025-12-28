2b3b2115e1b4ccf9c2b8be393706aa72a3d345c1
vippermand <truelsandthetruels@gmail.com>
2025-12-28 21:32:00 +0100

    result update

--- Workspace Razor/wwwroot/js/jsSessionsStorage.js
+++ Workspace Razor/wwwroot/js/jsSessionsStorage.js
@@ -67,10 +67,10 @@ function allStorage() {
             "Opgave 4b svar": "correct",
             "Opgave 4c svar": "correct",
             "Opgave 4d svar": "correct",
-            "Ingredient1": ["🥚Æg", "🧂Salt"],
-            "Ingredient2": ["🧂Salt", "🥚Æg"],
-            "Ingredient3": "🫒Olie",
-            "Ingredient4": "🍶Eddike"
+            "Ingredient1": ["Æg", "Salt"],
+            "Ingredient2": ["Salt", "Æg"],
+            "Ingredient3": "Olie",
+            "Ingredient4": "Eddike"
         };
 
         const container = document.querySelector('.CheckAnswers');
